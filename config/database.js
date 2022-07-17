fcba4419031696ccbf79fa6fca10657e65f6f979
Naimur Rahman <64221853+mohammad-naimur-rahman@users.noreply.github.com>
2022-07-17 16:34:32 +0600

    connected sucessfully with cloud database

--- config/database.js
+++ config/database.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("DATABASE_HOST", "127.0.0.1"),
-      port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "portfolioblog"),
-      user: env("DATABASE_USERNAME", "postgres"),
-      password: env("DATABASE_PASSWORD", "12345"),
+      host: env("DATABASE_HOST"),
+      port: env.int("DATABASE_PORT"),
+      database: env("DATABASE_NAME"),
+      user: env("DATABASE_USERNAME"),
+      password: env("DATABASE_PASSWORD"),
       ssl: process.env.DATABASE_URL ? true : false,
     },
   },
